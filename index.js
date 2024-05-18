97871282149fddecbc7b7f5606ae2322468ff43e
harshrajshs <106874062+harshrajshs@users.noreply.github.com>
2024-05-18 10:50:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
   origin: [
-    "http://localhost:3000",
     "https://srijanhomeschool.netlify.app",
   ],
   credentials: true,
